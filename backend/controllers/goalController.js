1a508406af53ea7d510b0d2e29c147a3fd3b3c06
laminemessaci <lamine.messaci18@gmail.com>
2022-10-04 19:25:56 +0200

    Add error-middleware-handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,12 @@ const getGoals = (req, res) => {
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req, res) => {
-  res.status(200).json({ message: `Ubdate Goal ${req.params.id}` });
+  // console.log(req.body);
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field.");
+  }
+  res.status(200).json({ message: `Set Goal` });
 };
 
 // @desc    Update goal
@@ -23,7 +28,7 @@ const updateGoal = (req, res) => {
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = (req, res) => {
-  res.status(200).json({ message: `Ubdate Goal ${req.params.id}` });
+  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
 };
 
 module.exports = {
