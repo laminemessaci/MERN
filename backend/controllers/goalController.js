11429c777c5d7074f9cbe6f77c0e5580be2a3f06
laminemessaci <lamine.messaci18@gmail.com>
2022-10-04 18:54:51 +0200

    refact code

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,34 @@
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
 
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: `Ubdate Goal ${req.params.id}` });
+};
+
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Ubdate Goal ${req.params.id}` });
+};
+
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Ubdate Goal ${req.params.id}` });
+};
+
 module.exports = {
   getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
 };
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals } = require("../controllers/goalController");
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.get("/", getGoals);
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goals" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Ubdate Goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
-});
+// router.get("/", getGoals);
+// router.put("/:id", updateGoal);
+// router.delete("/:id", deleteGoal);
+// router.post("/", setGoal);
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").delete(deleteGoal).put(updateGoal);
 
 module.exports = router;
